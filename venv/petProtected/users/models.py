53f739978c6ee6203652159145c6707d3707fd6a
Lin Bokobza <linbok123@gmail.com>
2021-11-29 19:15:49 +0200

    Lin's edited files

---
+++ venv/petProtected/users/models.py
@@ -0,0 +1,23 @@
+# from django.db import models
+
+# # Create your models here.
+
+# class login(models.Model):
+#     email=models.EmailField(max_length=100)
+#     password=models.CharField(max_length=20)
+
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# # Create your models here.
+# class User(models.Model):
+#     username=models.CharField(max_length=20, null=True)
+#     name=models.CharField(max_length=20, null=True)
+#     lastName=models.CharField(max_length=20, null=True)
+#     phone=models.IntegerField(default=0, null=True)
+#     adress=models.CharField(max_length=50,null=True)
+#     email=models.CharField(max_length=50, null=True)
+#     password=models.CharField(max_length=20,null=True)
